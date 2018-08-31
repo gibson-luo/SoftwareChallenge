f472189182c6850987b1ac2773c2396b71435800
Gibson Luo <lzrgibson@icloud.com>
2018-08-31 12:38:07 -0400

    an example for trying to get data from LCBO api

--- vinego-api/app/controllers/HomeController.java
+++ vinego-api/app/controllers/HomeController.java
@@ -1,12 +1,24 @@
 package controllers;
 
-import play.mvc.*;
+import java.util.concurrent.CompletionStage;
+
+import com.google.inject.Inject;
+import play.libs.ws.WSBodyReadables;
+import play.libs.ws.WSClient;
+import play.libs.ws.WSRequest;
+import play.mvc.Controller;
+import play.mvc.Result;
 
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
-public class HomeController extends Controller {
+public class HomeController extends Controller implements WSBodyReadables {
+
+    @Inject
+    WSClient ws;
+
+    private static String ACCESS_KEY = "MDplOTU2YjM3NC1hYzYwLTExZTgtYTQ0ZS0zYjZlMjk2ZTI4YzY6NEl2WTVsem00R0E1SDNwSnBjTUhjaW03anFRZjJwSGFYTTBh";
 
     /**
      * An action that renders an HTML page with a welcome message.
@@ -16,6 +28,24 @@ public class HomeController extends Controller {
      */
     public Result index() {
         return ok(views.html.index.render());
+    }
+
+    /**
+     * try to get the product list from LCBO API
+     * @return
+     */
+    public CompletionStage<Result> products() {
+
+        WSRequest request = ws.url("http://lcboapi.com/products");
+        request.addQueryParameter("access_key", ACCESS_KEY)
+            .addQueryParameter("page", "1")
+            .addQueryParameter("per_page", "10");
+
+        return request.get()
+            .thenApply(response ->
+                ok(response.asJson())
+            );
+
     }
 
 }
